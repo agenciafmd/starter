5c795366bfc46ec00115ce03dd3435a67445691a
Irineu Junior <irineu@fmd.ag>
2019-08-08 16:18:42 -0300

    feat(starter): libera para os primeiros testes

--- public/js/admix.js
+++ public/js/admix.js
@@ -374,41 +374,41 @@ if (token) {
       };
     },
     layoutTemplates: {
-      fileIcon: '<i class="fe fe-file kv-caption-icon"></i> ',
+      fileIcon: '<i class="icon fe-file kv-caption-icon"></i> ',
       main1: "{preview}\n" + "<div class='input-group {class}'>\n" + "   <div class='input-group-btn input-group-prepend'>\n" + "       {browse}\n" + "       {upload}\n" + "   </div>\n" + "   {caption}\n" + "</div>",
-      actions: '<div class="file-actions">\n' + '    <div class="file-footer-buttons">\n' + '       {download} ' + '       {upload} ' + '       {delete} ' + '       {zoom} ' + '       {other} ' + '       <button type="button" class="btn btn-sm btn-kv btn-outline-secondary kv-file-tags" title="Alterar descrição"><i class="fe fe-tag"></i></button>' + '    </div>\n' + '</div>\n' + '{drag}\n' + '<div class="clearfix"></div>',
+      actions: '<div class="file-actions">\n' + '    <div class="file-footer-buttons">\n' + '       {download} ' + '       {upload} ' + '       {delete} ' + '       {zoom} ' + '       {other} ' + '       <button type="button" class="btn btn-sm btn-kv btn-outline-secondary kv-file-tags" title="Alterar descrição"><i class="icon fe-tag"></i></button>' + '    </div>\n' + '</div>\n' + '{drag}\n' + '<div class="clearfix"></div>',
       size: '<span>{sizeText}</span>'
     },
     fileActionSettings: {
       showUpload: false,
       showDrag: false,
-      removeIcon: '<i class="fe fe-trash"></i>',
-      uploadIcon: '<i class="fe fe-upload"></i>',
-      uploadRetryIcon: '<i class="fe fe-repeat"></i>',
-      downloadIcon: '<i class="fe fe-download"></i>',
-      zoomIcon: '<i class="fe fe-zoom-in"></i>',
-      dragIcon: '<span class="btn btn-sm btn-kv btn-outline-secondary"><i class="fe fe-move"></i></span>',
-      indicatorNew: '<span class="btn btn-sm btn-kv btn-outline-warning"><i class="fe fe-star"></i></span>',
-      indicatorSuccess: '<span class="btn btn-sm btn-kv btn-outline-success"><i class="fe fe-check"></i></span>',
-      indicatorError: '<span class="btn btn-sm btn-kv btn-outline-danger"><i class="fe fe-alert-triangle"></i></span>',
-      indicatorLoading: '<span class="btn btn-sm btn-kv btn-outline-secondary"><i class="fe fe-spinner fe-loader"></i></span>',
-      indicatorPaused: '<i class="fe fe-pause text-info"></i>'
+      removeIcon: '<i class="icon fe-trash"></i>',
+      uploadIcon: '<i class="icon fe-upload"></i>',
+      uploadRetryIcon: '<i class="icon fe-repeat"></i>',
+      downloadIcon: '<i class="icon fe-download"></i>',
+      zoomIcon: '<i class="icon fe-zoom-in"></i>',
+      dragIcon: '<span class="btn btn-sm btn-kv btn-outline-secondary"><i class="icon fe-move"></i></span>',
+      indicatorNew: '<span class="btn btn-sm btn-kv btn-outline-warning"><i class="icon fe-star"></i></span>',
+      indicatorSuccess: '<span class="btn btn-sm btn-kv btn-outline-success"><i class="icon fe-check"></i></span>',
+      indicatorError: '<span class="btn btn-sm btn-kv btn-outline-danger"><i class="icon fe-alert-triangle"></i></span>',
+      indicatorLoading: '<span class="btn btn-sm btn-kv btn-outline-secondary"><i class="icon fe-spinner fe-loader"></i></span>',
+      indicatorPaused: '<i class="icon fe-pause text-info"></i>'
     },
     previewZoomButtonIcons: {
-      prev: '<i class="fe fe-caret-left fe-lg"></i>',
-      next: '<i class="fe fe-caret-right fe-lg"></i>',
-      toggleheader: '<i class="fe fe-fw fe-minimize-2"></i>',
-      fullscreen: '<i class="fe fe-fw fe-maximize-2"></i>',
-      borderless: '<i class="fe fe-fw fe-external-link"></i>',
-      close: '<i class="fe fe-fw fe-x"></i>'
+      prev: '<i class="icon fe-caret-left fe-lg"></i>',
+      next: '<i class="icon fe-caret-right fe-lg"></i>',
+      toggleheader: '<i class="icon fe-fw fe-minimize-2"></i>',
+      fullscreen: '<i class="icon fe-fw fe-maximize-2"></i>',
+      borderless: '<i class="icon fe-fw fe-external-link"></i>',
+      close: '<i class="icon fe-fw fe-x"></i>'
     },
-    previewFileIcon: '<i class="fe fe-file"></i>',
-    browseIcon: '<i class="fe fe-folder-open"></i>',
-    removeIcon: '<i class="fe fe-trash"></i>',
-    cancelIcon: '<i class="fe fe-ban"></i>',
-    pauseIcon: '<i class="fe fe-pause"></i>',
-    uploadIcon: '<i class="fe fe-upload"></i>',
-    msgValidationErrorIcon: '<i class="fe fe-exclamation-circle"></i> '
+    previewFileIcon: '<i class="icon fe-file"></i>',
+    browseIcon: '<i class="icon fe-folder-open"></i>',
+    removeIcon: '<i class="icon fe-trash"></i>',
+    cancelIcon: '<i class="icon fe-ban"></i>',
+    pauseIcon: '<i class="icon fe-pause"></i>',
+    uploadIcon: '<i class="icon fe-upload"></i>',
+    msgValidationErrorIcon: '<i class="icon fe-exclamation-circle"></i> '
   };
 })(window.jQuery);
 
