f990e6d2e1e6340ad731d5ec3690749efc9f95aa
Irineu Martins Junior <irineujunior@gmail.com>
2020-02-05 08:30:08 -0300

    chore(br): remove quebra de linha

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Página expirada'))
 @section('code', '419')
-@section('message', __('A página expirou por inatividade.<br />Por favor, volte para a página inicial e tente novamente.'))
+@section('message', __('A página expirou por inatividade. Por favor, volte para a página inicial e tente novamente.'))
