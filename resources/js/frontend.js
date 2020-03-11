0fc15ad1b881a11f37b7a876a3f3f1dd45ba9314
Camila Maria Rezende de Paiva <camila@fmd.ag>
2020-03-11 16:47:19 -0300

    chore(scrollbar): Altera operador da condicao do if de != para !==;

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -179,17 +179,18 @@ function setupSmoothScroll() {
 function verifyUserAgent() {
 
   var OSNome = "";
-  if (window.navigator.userAgent.indexOf("Windows NT 10.0")!= -1) OSNome="Windows 10";
-  if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1) OSNome="Windows 8";
-  if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1) OSNome="Windows 7";
-  if (window.navigator.userAgent.indexOf("Windows NT 6.0") != -1) OSNome="Windows Vista";
-  if (window.navigator.userAgent.indexOf("Windows NT 5.1") != -1) OSNome="Windows XP";
-  if (window.navigator.userAgent.indexOf("Windows NT 5.0") != -1) OSNome="Windows 2000";
-  if (window.navigator.userAgent.indexOf("Mac")            != -1) OSNome="Mac/iOS";
-  if (window.navigator.userAgent.indexOf("X11")            != -1) OSNome="UNIX";
-  if (window.navigator.userAgent.indexOf("Linux")          != -1) OSNome="Linux";
+  if (window.navigator.userAgent.indexOf("Windows NT 10.0")!== -1) OSNome="Windows 10";
+  if (window.navigator.userAgent.indexOf("Windows NT 6.2") !== -1) OSNome="Windows 8";
+  if (window.navigator.userAgent.indexOf("Windows NT 6.1") !== -1) OSNome="Windows 7";
+  if (window.navigator.userAgent.indexOf("Windows NT 6.0") !== -1) OSNome="Windows Vista";
+  if (window.navigator.userAgent.indexOf("Windows NT 5.1") !== -1) OSNome="Windows XP";
+  if (window.navigator.userAgent.indexOf("Windows NT 5.0") !== -1) OSNome="Windows 2000";
+  if (window.navigator.userAgent.indexOf("Mac")            !== -1) OSNome="Mac/iOS";
+  if (window.navigator.userAgent.indexOf("X11")            !== -1) OSNome="UNIX";
+  if (window.navigator.userAgent.indexOf("Linux")          !== -1) OSNome="Linux";
 
-  if (OSNome != "Mac/iOS") {
+  if (OSNome !== "Mac/iOS") {
+
     let body = document.querySelector('body');
     body.classList.add("style-scroll");
   }
