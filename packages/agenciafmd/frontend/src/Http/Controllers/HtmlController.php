0481d0608be285f7ce5a7f75cd0d8fb0f605ff90
Irineu Junior <irineu@fmd.ag>
2020-04-24 08:26:28 -0300

    feat(html): força o redirect quando estiver em produção

--- packages/agenciafmd/frontend/src/Http/Controllers/HtmlController.php
+++ packages/agenciafmd/frontend/src/Http/Controllers/HtmlController.php
@@ -8,11 +8,15 @@ class HtmlController extends Controller
 {
     public function index($file = 'index')
     {
+        if (app()->environment(['production'])) {
+            return redirect('/', 301);
+        }
+
         $path = base_path('packages/agenciafmd/frontend/src/resources/views/html/');
         if (!file_exists($path . $file . '.blade.php')) {
             $file = 'index';
         }
 
-        return  view('agenciafmd/frontend::html.' . $file);
+        return view('agenciafmd/frontend::html.' . $file);
     }
 }
