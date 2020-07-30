85b0d8dd721ef3a5a68407a740ffa4a9419e37b6
renanferro <renan.ferro@fmd.ag>
2020-07-30 14:29:02 -0300

    fix(meta tag) adiciona atributo de maximum-scale dentro da meta tag na master e /html/master

--- packages/agenciafmd/frontend/src/resources/views/master.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/master.blade.php
@@ -21,7 +21,7 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5.0">
 
     <title>@yield('title', '') | {{ config('app.name') }}</title>
     <meta name="description" content="@yield('description', '')">
