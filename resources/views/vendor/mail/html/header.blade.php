ea91d2a7b95a78d20cc5d70c3490572c5dde43a1
Irineu Junior <irineu@mixd.com.br>
2020-11-25 17:56:45 -0300

    feat(forms): limpeza dos forms

--- resources/views/vendor/mail/html/header.blade.php
+++
@@ -1,8 +0,0 @@
-<tr>
-    <td class="header">
-        <a href="{{ $url }}">
-            <img src="{{ asset('/images/logo-email.png') }}" style="">
-{{--            {{ $slot }}--}}
-        </a>
-    </td>
-</tr>
