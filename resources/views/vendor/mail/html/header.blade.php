eb8ec2d1dee353e8ff897ccb42e57299084f6d82
Irineu Junior <irineu@mixd.com.br>
2020-11-26 14:57:02 -0300

    Revert "feat(forms): limpeza dos forms"
    This reverts commit ea91d2a7

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}">
+            <img src="{{ asset('/images/logo-email.png') }}" style="">
+{{--            {{ $slot }}--}}
+        </a>
+    </td>
+</tr>
