8659aeaa6803d00eccf885de74e19b45cbcfc590
Irineu Junior <irineu@mixd.com.br>
2021-06-22 17:09:29 -0300

    feat(sitemap) adiciona o redirect do sitemap para o storage

--- packages/agenciafmd/frontend/src/routes/web.php
+++ packages/agenciafmd/frontend/src/routes/web.php
@@ -2,6 +2,9 @@
 
 use Agenciafmd\Frontend\Http\Controllers\FrontendController;
 use Agenciafmd\Frontend\Http\Controllers\HtmlController;
+use Illuminate\Support\Facades\Storage;
+
+Route::redirect('/sitemap.xml', Storage::url('sitemap.xml'), 301);
 
 Route::get('html/{any?}', [HtmlController::class, 'index'])
     ->name('frontend.html');
