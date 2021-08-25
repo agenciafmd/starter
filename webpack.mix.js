b90dda25e85191cd0c7dd2083766520034ae4669
Vitor Hugo F Lopes <vitor@fmd.ag>
2021-08-25 15:59:52 -0300

    feat(laravel-mix-purgecss): Adiciona nova chamada da versão ^6.*

--- webpack.mix.js
+++ webpack.mix.js
@@ -39,21 +39,22 @@ const wpConfig = {
 mix
     .sass('resources/sass/frontend.scss', 'public/css')
     .purgeCss({
-      enabled: mix.inProduction(),
-      globs: [
-        path.join(__dirname, 'packages/agenciafmd/frontend/src/**/*.php'),
-        path.join(__dirname, 'node_modules/@fancyapps/fancybox/dist/*.js'),
-        path.join(__dirname, 'node_modules/swiper/**/*.js'),
-        path.join(__dirname, 'node_modules/jquery/dist/jquery.min.js'),
-        path.join(__dirname, 'node_modules/select2/dist/**/*.js'),
-        path.join(__dirname, 'node_modules/sweetalert2/dist/*.js'),
-        path.join(
-            __dirname,
-            'node_modules/bootstrap/dist/js/bootstrap.min.js',
-        ),
-      ],
-      // Include classes we don't have direct access
-      whitelistPatterns: [/hs-*/, /tns-*/, /js-*/, /swiper-*/],
+      extend: {
+        content: [
+          path.join(__dirname, 'packages/agenciafmd/frontend/src/**/*.php'),
+          path.join(__dirname, 'node_modules/@fancyapps/fancybox/dist/*.js'),
+          path.join(__dirname, 'node_modules/swiper/**/*.js'),
+          path.join(__dirname, 'node_modules/jquery/dist/jquery.min.js'),
+          path.join(__dirname, 'node_modules/select2/dist/**/*.js'),
+          path.join(__dirname, 'node_modules/sweetalert2/dist/*.js'),
+          path.join(
+              __dirname,
+              'node_modules/bootstrap/dist/js/bootstrap.min.js',
+          ),
+        ],
+        // Include classes we don't have direct access
+        safelist: [/hs-*/, /tns-*/, /js-*/, /swiper-*/],
+      },
     })
     .options({
       imgLoaderOptions: {
