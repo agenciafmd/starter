f6c11f1b63bdf2be6feb79dfb04e53620c9e93d5
Renan Breno <renan.breno@fmd.ag>
2021-09-10 02:08:32 -0300

    chore(public): Atualiza public

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -14998,7 +14998,7 @@ function setupInfiniteScroll() {
 }
 
 function setupShareWindow() {
-  $('.share').click(function (e) {
+  $('.js-btn-share').click(function (e) {
     e.preventDefault();
     window.open($(this).attr('href'), 'shareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
     return false;
