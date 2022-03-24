5fae95c1965314c674cceb42d82f8121e2e21d6a
Renan Breno <renan.breno@fmd.ag>
2022-03-24 14:05:41 -0300

    feat(components): Adiciona o componente de breadcrumb

---
+++ packages/agenciafmd/frontend/src/resources/views/components/breadcrumb.blade.php
@@ -0,0 +1,27 @@
+@props([
+    'list',
+])
+
+<nav aria-label="breadcrumb" {{ $attributes }}>
+    <ol class="breadcrumb">
+        @foreach ($list as $title => $link)
+            @if(!$loop->last)
+                <li class="breadcrumb-item"><a href="{{ $link }}">{{ $title }}</a></li>
+            @else
+                <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
+            @endif
+        @endforeach
+    </ol>
+</nav>
+
+{{-- Example:
+    @php
+        $breadcrumb = [
+            'Home' => '/html',
+            'Produtos' => '/html/produtos',
+            'Categoria 1' => '/html/produtos/categoria-1',
+            'Nome do produto' => '',
+        ];
+    @endphp
+    <x-frontend::breadcrumb :list="$breadcrumb" class="py-2"/>
+--}}
