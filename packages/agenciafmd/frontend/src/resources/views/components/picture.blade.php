477ac6a55a92efd0e76db108fd2ee88f6ca06ec4
Renan Breno <renan.breno@fmd.ag>
2022-04-26 18:12:57 -0300

    feat(components): Padroniza componentes relacionados às imagens

--- packages/agenciafmd/frontend/src/resources/views/components/picture.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/components/picture.blade.php
@@ -1,26 +1,24 @@
-@if($fancybox ?? false)
-    <a href="{{ $image }}"
-       data-fancybox="{{ $fancybox }}"
-       aria-label="{{ $alt }}"
-       data-caption="{{ $alt }}"
-       class="{{ $fancyboxClass }}">
-        @endif
-        <picture class="{{ $pictureClass }}">
-            @foreach($sources as $source)
-                @if(!$loop->last)
-                    <source media="(min-width: {{ $source['name'] }}px)"
-                            srcset="{{ $source['default'] }}, {{ $source['retina'] }} 2x">
-                @else
-                    <source media="(max-width: {{ ($sources->take(-2)->first()['name']-1) }}px)"
-                            srcset="{{ $source['default'] }}, {{ $source['retina'] }} 2x">
-                @endif
-            @endforeach
-            <img loading="lazy"
-                 width="{{ $w ?? '1' }}"
-                 height="{{ $h ?? '1' }}"
-                 src="data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-                 class="{{ $imgClass ?: 'img-sanitize img-cover' }}"
-                 alt="{{ $alt ?? 'sem imagem' }}"
-                 title="{{ $alt ?? 'sem imagem' }}">
-        </picture>
-        @if($fancybox ?? false) </a> @endif
+@props([
+    'image',
+    'title',
+    'alt' => $title,
+    'pictureClass' => null,
+    'hasBreakpoints' => false,
+    'breakpointDesktopWidth' => null,
+    'breakpointDesktopSuffix' => null,
+    'breakpointNotebookWidth' => null,
+    'breakpointNotebookSuffix' => null,
+])
+
+<picture class="{{ $pictureClass }}">
+    <x-dynamic-component :component="$hasBreakpoints ? 'frontend::sources' : 'frontend::single-source'"
+                         :image="$image"
+                         :title="$title"
+                         :alt="$alt"
+                         :breakpointDesktopWidth="$breakpointDesktopWidth"
+                         :breakpointDesktopSuffix="$breakpointDesktopSuffix"
+                         :breakpointNotebookWidth="$breakpointNotebookWidth"
+                         :breakpointNotebookSuffix="$breakpointNotebookSuffix"
+    
+                         {{ $attributes }}></x-dynamic-component>
+</picture>
