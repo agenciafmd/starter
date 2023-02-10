377ec7b95c21ed40266be2c58a955ddf61359dcf
gustavo <gustavocpsalles22@gmail.com>
2023-02-10 09:27:05 -0300

    chore(Swiper buttons): Ajusta o componente de swiper buttons

--- packages/agenciafmd/frontend/src/resources/views/components/swiper-buttons.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/components/swiper-buttons.blade.php
@@ -1,14 +1,2 @@
-@props([
-'class' => '',
-'paginationClass' => '',
-'pagination' => false,
-'classButtonNext' => '',
-'classButtonPrev' => '',
-])
-
-<div class="swiper-button-prev {{ $classButtonPrev }} {{ $class }}"></div>
-<div class="swiper-button-next {{ $classButtonNext }} {{ $class }}"></div>
-
-@if($pagination)
-<div class="{{ $paginationClass }} swiper-pagination"></div>
-@endif
+<div class="swiper-button-prev"></div>
+<div class="swiper-button-next"></div>
