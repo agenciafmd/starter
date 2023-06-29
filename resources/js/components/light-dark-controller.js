3e5a2331328f99ed4c43af4cc86a09318ea88dd2
Renan Ferro <renan.ferro@fmd.ag>
2023-06-29 15:25:40 -0300

    feat(estrutura): implementa estrutura de funcionamento para componente de toggle theme

---
+++ resources/js/components/light-dark-controller.js
@@ -0,0 +1,50 @@
+function setupLightDarkModeController() {
+
+  const toggleLightDarkTheme = document.getElementById('switchControllerLightDarkTheme');
+
+  if (!toggleLightDarkTheme) {
+    throw new Error(
+      'Sorry, we couldn\'t find toggle element for light and dark theme :(');
+  }
+
+  // Gets the theme value saved in localStorage
+  const getStoragedTheme = () => localStorage.getItem('theme');
+  // Defines a new theme item by storing the theme type in localStorage
+  const setStoragedTheme = theme => localStorage.setItem('theme', theme);
+
+  toggleLightDarkTheme.addEventListener('change', (event) => {
+
+    event.target.checked ? activePreferedTheme('dark') : activePreferedTheme('light');
+  });
+
+  // Enables the customer's preferred theme type
+  function activePreferedTheme(theme) {
+
+    setStoragedTheme(theme);
+    setDataBSThemeAttributeInDocument(theme);
+  }
+
+  // Set data-bs-theme=theme in Document
+  function setDataBSThemeAttributeInDocument(theme) {
+
+    document.documentElement.setAttribute('data-bs-theme', theme);
+  }
+
+  // Check the client's theme preference in the localStorage
+  function verifyPreferenceClientTheme() {
+
+    activePreferedTheme(getStoragedTheme());
+  }
+
+  // Check if the theme is dark to set the toggle as active or not
+  function updateToggleThemeStatus() {
+
+    getStoragedTheme() === 'dark' ? toggleLightDarkTheme.checked = true : toggleLightDarkTheme.checked = false;
+  }
+
+  window.addEventListener('load', () => {
+
+    updateToggleThemeStatus();
+    verifyPreferenceClientTheme();
+  });
+}
