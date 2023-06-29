5488ed0abd44529224bbabb449e7e95ac8863234
Renan Ferro <renan.ferro@fmd.ag>
2023-06-29 15:23:08 -0300

    feat(theme toggle): implementa novo componente para toggle de tema

---
+++ packages/agenciafmd/frontend/src/resources/views/components/light-dark-toggle.blade.php
@@ -0,0 +1,8 @@
+<div class="ms-lg-1 light-dark-toggle form-check form-switch">
+    <input class="form-check-input"
+           type="checkbox"
+           role="switch"
+           id="switchControllerLightDarkTheme">
+    <label class="form-check-label"
+           for="switchControllerLightDarkTheme"></label>
+</div>
