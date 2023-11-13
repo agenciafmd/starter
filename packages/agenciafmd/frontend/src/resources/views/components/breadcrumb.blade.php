cadf6cdb1a16887137c5b5147df99131161c22f3
Vitor Hugo F Lopes <vitor.automec@hotmail.com>
2023-11-13 18:45:43 -0300

    feat(breadcrumb): Adiciona tema dark

--- packages/agenciafmd/frontend/src/resources/views/components/breadcrumb.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/components/breadcrumb.blade.php
@@ -1,15 +1,16 @@
 @props([
 'list',
+'isDarkMode' => null,
 ])
 
 <nav aria-label="breadcrumb" {{ $attributes }}>
-    <ol class="breadcrumb">
+    <ol class="breadcrumb @if($isDarkMode) breadcrumb-dark @endif">
         @foreach ($list as $title => $link)
-            @if(!$loop->last)
-                <li class="breadcrumb-item"><a href="{{ $link }}">{{ $title }}</a></li>
-            @else
-                <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
-            @endif
+        @if(!$loop->last)
+        <li class="breadcrumb-item"><a href="{{ $link }}" title="{{ $title }}">{{ $title }}</a></li>
+        @else
+        <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
+        @endif
         @endforeach
     </ol>
 </nav>
