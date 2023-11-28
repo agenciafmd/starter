3c1481cb43b4221dd29211ef09eb999aac038911
Patrick Bandeira <patrick010509@gmail.com>
2023-11-28 16:16:16 -0300

    feat(novo component): retira campos e substitui por attributes

--- packages/agenciafmd/frontend/src/resources/views/components/pdf.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/components/pdf.blade.php
@@ -1,17 +1,12 @@
 @props([
 'name',
-'class' => null,
 'label',
-'title' => 'Clique para acessar agora o pdf',
-'arialabel' => 'Link para visualização do pdf',
 'download' => false,
 ])
 
 <a href="{{ Str::startsWith(request()->route()->getName(), 'frontend.html') ? '/pdf/' . $name : mix('/pdf/' . $name) }}"
-   title="{{ $title }}"
-   aria-label="{{ $arialabel }}"
    @if($download) download @else target="_blank" @endif
-   class="{{ $class }}">
+   {{ $attributes }}>
   <span aria-hidden="true">
     {{ $label }}
   </span>
