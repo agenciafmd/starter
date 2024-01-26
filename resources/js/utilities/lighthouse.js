1580381600196127b014632489832dbe6382dab5
Camila Paiva <camila@fmd.ag>
2024-01-26 11:39:14 -0300

    feat(lighthouse): Adiciona nova página para visualizar os reports de todas as páginas;

--- resources/js/utilities/lighthouse.js
+++ resources/js/utilities/lighthouse.js
@@ -1,24 +1,161 @@
 require('dotenv').config();
 const exec = require("child_process").exec;
+const fs = require("fs");
 
-const date = new Date();
+let HTMLPagesFiles = fs.readdirSync('packages/agenciafmd/frontend/src/resources/views/html');
 
-const projectURL = process.env.APP_URL;
-const chromeFlags = '--headless --no-sandbox';
-const reportDate = [date.getDate(), (date.getMonth() + 1), date.getFullYear(), (date.getHours() + 'h'), (date.getMinutes() + 'm')].join('-');
-const reportPath = `./resources/lighthouse/lighthouse-report-${reportDate}.html`;
-const createLighthouseReportFolder = 'mkdir -p ./resources/lighthouse';
-const runLighthouse = `lighthouse ${projectURL} --chrome-flags="${chromeFlags}" --output-path=${reportPath}`;
+let allReportsPerPage = [];
 
 console.log("🌀 Iniciando lighthouse...");
-exec(`${createLighthouseReportFolder} && ${runLighthouse}`, function (error, stdout) {
 
-  console.log(stdout);
+function formatPageArrayFiles() {
+  
+  const unwantedPagesFiles = [
+    'internal',
+    'master',
+    'partials',
+    'template',
+    'web-vitals',
+    'tema'
+  ];
+  
+  HTMLPagesFiles = HTMLPagesFiles.map(page => page.replace('.blade.php', ''))
+    .filter(page => !unwantedPagesFiles.includes(page));
+}
 
-  if (error) {
-    console.error(error);
-    return;
+formatPageArrayFiles();
+
+function convertToPercentage(number) {
+  return Math.round(number * 100)
+}
+
+function scoreStatus(score) {
+  
+  const scoreConvertedToPercentage = convertToPercentage(score);
+  
+  let reportComplete = {
+    score: scoreConvertedToPercentage,
+    status: ''
   }
+  
+  const status = {
+    dangerous: scoreConvertedToPercentage <= 25,
+    warning: 25 < scoreConvertedToPercentage <= 50,
+    info: 50 < scoreConvertedToPercentage <= 80,
+    success: 80 < scoreConvertedToPercentage
+  }
+  
+  if (status.dangerous) {
+    reportComplete.status = 'dangerous';
+  }
+  
+  if (status.warning) {
+    reportComplete.status = 'warning';
+  }
+  
+  if (status.info) {
+    reportComplete.status = 'info';
+  }
+  
+  if (status.success) {
+    reportComplete.status = 'success';
+  }
+  
+  return reportComplete;
+}
 
-  console.log("✅ Auditoria concluída.");
-});
+function generateReports() {
+  return new Promise((resolve) => {
+    
+    const createLighthouseReportFolder = 'rm -rf ./public/web-vitals/ && mkdir -p ./public/web-vitals';
+    
+    exec(createLighthouseReportFolder, function (error, stdout) {
+      
+      console.log(stdout);
+      
+      if (error) {
+        console.error(error);
+        return;
+      }
+      
+      HTMLPagesFiles.forEach((page, index) => {
+        
+        const projectURL = `${process.env.APP_URL}/html/${page}`;
+        const chromeFlags = '--headless --no-sandbox';
+        const reportPath = `./public/web-vitals/${page}`;
+        
+        const runLighthouse = `lighthouse ${projectURL} --chrome-flags="${chromeFlags}" --output='json,html' --output-path=${reportPath}`;
+        
+        exec(runLighthouse, function (error, stdout) {
+          
+          console.log(stdout);
+          
+          if (error) {
+            console.error(error);
+            return;
+          }
+          
+          fs.readFile(`${reportPath}.report.json`, "utf8", (error, data) => {
+            if (error) {
+              console.log(error);
+              return;
+            }
+            
+            const report = JSON.parse(data).categories;
+            const reportComplete = {
+              pageName: page,
+              reports: {
+                performance: {
+                  label: 'Performance',
+                  score: scoreStatus(report.performance.score).score,
+                  status: scoreStatus(report.performance.score).status
+                },
+                accessibility: {
+                  label: 'Acessibilidade',
+                  score: scoreStatus(report.accessibility.score).score,
+                  status: scoreStatus(report.accessibility.score).status
+                },
+                bestPractices: {
+                  label: 'Acessibilidade',
+                  score: scoreStatus(report['best-practices'].score).score,
+                  status: scoreStatus(report['best-practices'].score).status
+                },
+                seo: {
+                  label: 'Acessibilidade',
+                  score: scoreStatus(report.seo.score).score,
+                  status: scoreStatus(report.seo.score).status
+                },
+                pwa:{
+                  label: 'Acessibilidade',
+                  score: scoreStatus(report.pwa.score).score,
+                  status: scoreStatus(report.pwa.score).status
+                },
+              }
+            }
+            
+            allReportsPerPage.push(reportComplete);
+            
+            const hasAllReports = index === (HTMLPagesFiles.length - 1);
+            
+            if (hasAllReports) {
+              resolve(allReportsPerPage);
+            }
+          });
+        });
+      });
+    });
+  });
+}
+
+generateReports().then((resolve) => {
+  
+  fs.writeFile('./public/web-vitals/web-vitals.json', JSON.stringify(resolve), (err) => {
+    
+    if (err) {
+      console.log(err);
+      return
+    }
+    
+    console.log(`✅ Reports adicionados com sucesso.`);
+  });
+})
