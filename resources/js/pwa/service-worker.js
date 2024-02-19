ca3a4afb58d5d3b24ffcca985f636ee515eb34e7
edmosouza <xedmox@gmail.com>
2024-02-19 18:34:47 -0300

    refactor(pwa): Ajusta pwa para ativar modo offline

--- resources/js/pwa/service-worker.js
+++ resources/js/pwa/service-worker.js
@@ -1,23 +1,70 @@
-const applicationName = "fmd-starter";
+const applicationName = 'cache-v1';
 const assets = [
-  "/",
-  "/index.html",
-  "/css/frontend.css",
-  "/js/frontend.js",
+  '/',
+  '/html',
+  '/css/frontend.css',
+  '/js/frontend.js',
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-      caches.open(applicationName).then(cache => {
-        cache.addAll(assets)
+self.addEventListener('install', (e) => {
+
+  console.log('[Service Worker] Install');
+
+  e.waitUntil(
+      (async () => {
+        const cache = await caches.open(applicationName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(assets);
+      })(),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+      (async () => {
+        const resource = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${ e.request.url }`);
+        if (resource) {
+          return resource;
+        }
+        const response = await fetch(e.request);
+        const cache = await caches.open(applicationName);
+        console.log(`[Service Worker] Caching new resource: ${ e.request.url }`);
+        await cache.put(e.request, response.clone());
+        return response;
+      })(),
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+            keyList.map((key) => {
+              if (key === applicationName) {
+                return;
+              }
+              return caches.delete(key);
+            }),
+        );
       }),
   );
 });
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      }),
-  );
-});
+//https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Tutorials/js13kGames/Offline_Service_workers
+
+// self.addEventListener("install", installEvent => {
+//   installEvent.waitUntil(
+//       caches.open(applicationName).then(cache => {
+//         cache.addAll(assets)
+//       }),
+//   );
+// });
+//
+// self.addEventListener("fetch", fetchEvent => {
+//   fetchEvent.respondWith(
+//       caches.match(fetchEvent.request).then(res => {
+//         return res || fetch(fetchEvent.request)
+//       }),
+//   );
+// });
