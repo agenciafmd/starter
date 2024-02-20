6c2f8fd8aaa23aa71cba660d56f97d563d598e61
Camila Paiva <camila@fmd.ag>
2024-02-20 08:34:22 -0300

    feat(lighthouse): Adiciona tabs para separar reports por ambiente;

--- packages/agenciafmd/frontend/src/resources/views/pages/web-vitals.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/pages/web-vitals.blade.php
@@ -37,60 +37,222 @@
       </div>
     </div>
     <section class="pt-block-small pt-md-block">
-      <div class="d-flex flex-column gap-2">
-        @forelse($webvitals as $webvital)
-        <div>
-          <h2 class="fs-6 fs-md-5">
-            /{{ $webvital['pageName'] }}
-          </h2>
-          <div class="row justify-content-md-between gx-md-3 gy-1h">
-            <div class="col-md order-md-1">
-              <div class="row gy-1">
-                @foreach($webvital['categories'] as $category)
-                <div class="col-md-3">
-                  <h3 class="fw-normal fs-small fs-md-base mb-0q">
-                    {{ $category['label'] }}
-                  </h3>
-                  <div class="d-flex gap-0h align-items-center">
+      <ul class="nav nav-tabs"
+          role="tablist">
+        <li class="nav-item"
+            role="presentation">
+          <a class="nav-link active"
+             data-bs-toggle="tab"
+             aria-label="Link Local"
+             title="Local"
+             href="#local"
+             aria-selected="true">
+            Local
+          </a>
+        </li>
+        <li class="nav-item"
+            role="presentation">
+          <a class="nav-link"
+             data-bs-toggle="tab"
+             aria-label="Link Homologação (.dev)"
+             title="Homologação (.dev)"
+             href="#hml">
+            Homologação (.dev)
+          </a>
+        </li>
+        <li class="nav-item"
+            role="presentation">
+          <a class="nav-link"
+             data-bs-toggle="tab"
+             aria-label="Link Produção"
+             title="Produção"
+             href="#prod">
+            Produção
+          </a>
+        </li>
+      </ul>
+      <div id="myTabContent"
+           class="tab-content">
+        <div class="tab-pane pt-2 fade active show"
+             id="local"
+             role="tabpanel">
+          <div class="d-flex flex-column gap-2">
+            @forelse($webvitals as $webvital)
+            <div>
+              <h2 class="fs-6 fs-md-5">
+                /{{ $webvital['pageName'] }}
+              </h2>
+              <div class="row justify-content-md-between gx-md-3 gy-1h">
+                <div class="col-md order-md-1">
+                  <div class="row gy-1">
+                    @foreach($webvital['categories'] as $category)
+                    <div class="col-md-3">
+                      <h3 class="fw-normal fs-small fs-md-base mb-0q">
+                        {{ $category['label'] }}
+                      </h3>
+                      <div class="d-flex gap-0h align-items-center">
                                             <span class="fs-small fw-bold">
                                                 {{ $category['score'] }}%
                                             </span>
-                    <div class="progress flex-grow-1"
-                         role="progressbar"
-                         aria-valuenow="{{ $category['score'] }}"
-                         aria-valuemin="0"
-                         aria-valuemax="100">
-                      <div class="progress-bar bg-{{ $category['status'] }}"
-                           style="width: {{ $category['score'] }}%"></div>
+                        <div class="progress flex-grow-1"
+                             role="progressbar"
+                             aria-valuenow="{{ $category['score'] }}"
+                             aria-valuemin="0"
+                             aria-valuemax="100">
+                          <div class="progress-bar bg-{{ $category['status'] }}"
+                               style="width: {{ $category['score'] }}%"></div>
+                        </div>
+                      </div>
                     </div>
+                    @endforeach
                   </div>
                 </div>
-                @endforeach
-              </div>
-            </div>
-            <div class="col-6 col-md-auto order-md-0">
-              <h3 class="fw-normal fs-small fs-md-base mb-0">
-                PWA
-              </h3>
-              <x-frontend::icon class="ic-status text-{{ $webvital['pwa'] }}"
-                                name="ic-web-vitals-{{ $webvital['pwa'] }}"/>
-            </div>
+                <div class="col-6 col-md-auto order-md-0">
+                  <h3 class="fw-normal fs-small fs-md-base mb-0">
+                    PWA
+                  </h3>
+                  <x-frontend::icon class="ic-status text-{{ $webvital['pwa'] }}"
+                                    name="ic-web-vitals-{{ $webvital['pwa'] }}"/>
+                </div>
 
-            <div class="col-6 col-md-auto align-self-end order-md-2">
-              <div class="d-grid">
-                <a href="/web-vitals/{{ $webvital['pageName'] }}.report.html"
-                   class="btn btn-outline-dark">
-                  ver detalhes
-                </a>
+                <div class="col-6 col-md-auto align-self-end order-md-2">
+                  <div class="d-grid">
+                    <a href="/web-vitals/{{ $webvital['pageName'] }}.report.html"
+                       class="btn btn-outline-dark">
+                      ver detalhes
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
+            @empty
+            <div class="bg-warning bg-opacity-25 p-1 rounded fs-small">
+              Nenhum dado encontrado. Por favor, rode <code class="px-0q">npm run lighthouse</code> para gerar os dados
+              necessários.
+            </div>
+            @endforelse
           </div>
         </div>
-        @empty
-        <div class="bg-warning bg-opacity-25 p-1 rounded fs-small">
-          Nenhum dado encontrado. Por favor, rode <code class="px-0q">npm run lighthouse</code> para gerar os dados necessários.
+        <div class="tab-pane pt-2 fade"
+             id="hml"
+             role="tabpanel">
+          <div class="d-flex flex-column gap-2">
+            @forelse($webvitals as $webvital)
+            <div>
+              <h2 class="fs-6 fs-md-5">
+                /{{ $webvital['pageName'] }}
+              </h2>
+              <div class="row justify-content-md-between gx-md-3 gy-1h">
+                <div class="col-md order-md-1">
+                  <div class="row gy-1">
+                    @foreach($webvital['categories'] as $category)
+                    <div class="col-md-3">
+                      <h3 class="fw-normal fs-small fs-md-base mb-0q">
+                        {{ $category['label'] }}
+                      </h3>
+                      <div class="d-flex gap-0h align-items-center">
+                                            <span class="fs-small fw-bold">
+                                                {{ $category['score'] }}%
+                                            </span>
+                        <div class="progress flex-grow-1"
+                             role="progressbar"
+                             aria-valuenow="{{ $category['score'] }}"
+                             aria-valuemin="0"
+                             aria-valuemax="100">
+                          <div class="progress-bar bg-{{ $category['status'] }}"
+                               style="width: {{ $category['score'] }}%"></div>
+                        </div>
+                      </div>
+                    </div>
+                    @endforeach
+                  </div>
+                </div>
+                <div class="col-6 col-md-auto order-md-0">
+                  <h3 class="fw-normal fs-small fs-md-base mb-0">
+                    PWA
+                  </h3>
+                  <x-frontend::icon class="ic-status text-{{ $webvital['pwa'] }}"
+                                    name="ic-web-vitals-{{ $webvital['pwa'] }}"/>
+                </div>
+
+                <div class="col-6 col-md-auto align-self-end order-md-2">
+                  <div class="d-grid">
+                    <a href="/web-vitals/{{ $webvital['pageName'] }}.report.html"
+                       class="btn btn-outline-dark">
+                      ver detalhes
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            @empty
+            <div class="bg-warning bg-opacity-25 p-1 rounded fs-small">
+              Nenhum dado encontrado. Por favor, rode <code class="px-0q">npm run lighthouse</code> para gerar os dados
+              necessários.
+            </div>
+            @endforelse
+          </div>
         </div>
-        @endforelse
+        <div class="tab-pane pt-2 fade"
+             id="prod"
+             role="tabpanel">
+          <div class="d-flex flex-column gap-2">
+            @forelse($webvitals as $webvital)
+            <div>
+              <h2 class="fs-6 fs-md-5">
+                /{{ $webvital['pageName'] }}
+              </h2>
+              <div class="row justify-content-md-between gx-md-3 gy-1h">
+                <div class="col-md order-md-1">
+                  <div class="row gy-1">
+                    @foreach($webvital['categories'] as $category)
+                    <div class="col-md-3">
+                      <h3 class="fw-normal fs-small fs-md-base mb-0q">
+                        {{ $category['label'] }}
+                      </h3>
+                      <div class="d-flex gap-0h align-items-center">
+                                            <span class="fs-small fw-bold">
+                                                {{ $category['score'] }}%
+                                            </span>
+                        <div class="progress flex-grow-1"
+                             role="progressbar"
+                             aria-valuenow="{{ $category['score'] }}"
+                             aria-valuemin="0"
+                             aria-valuemax="100">
+                          <div class="progress-bar bg-{{ $category['status'] }}"
+                               style="width: {{ $category['score'] }}%"></div>
+                        </div>
+                      </div>
+                    </div>
+                    @endforeach
+                  </div>
+                </div>
+                <div class="col-6 col-md-auto order-md-0">
+                  <h3 class="fw-normal fs-small fs-md-base mb-0">
+                    PWA
+                  </h3>
+                  <x-frontend::icon class="ic-status text-{{ $webvital['pwa'] }}"
+                                    name="ic-web-vitals-{{ $webvital['pwa'] }}"/>
+                </div>
+
+                <div class="col-6 col-md-auto align-self-end order-md-2">
+                  <div class="d-grid">
+                    <a href="/web-vitals/{{ $webvital['pageName'] }}.report.html"
+                       class="btn btn-outline-dark">
+                      ver detalhes
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            @empty
+            <div class="bg-warning bg-opacity-25 p-1 rounded fs-small">
+              Nenhum dado encontrado. Por favor, rode <code class="px-0q">npm run lighthouse</code> para gerar os dados
+              necessários.
+            </div>
+            @endforelse
+          </div>
+        </div>
       </div>
     </section>
   </div>
