8c41a80d91c38dc089eccff989c477ad6794a6be
Irineu Junior <irineu@fmd.ag>
2024-08-15 14:11:49 -0300

    feat(horizon) snapshot

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command('horizon:snapshot')
+    ->everyFiveMinutes();
