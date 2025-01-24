c47f9b9336ec6681293991c9a7830a28f9880b96
Irineu Junior <irineu@fmd.ag>
2025-01-24 17:29:21 -0300

    feat(frontend) move o vite para o frontend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::macro('image', fn (string $asset) => $this->asset("resources/images/{$asset}"));
-        Vite::macro('font', fn (string $asset) => $this->asset("resources/fonts/{$asset}"));
-        Vite::macro('sprite', fn (string $asset) => $this->asset('spritemap.svg') . "#sprite-{$asset}");
+        //
     }
 }
--- packages/agenciafmd/frontend/src/Providers/FrontendServiceProvider.php
+++ packages/agenciafmd/frontend/src/Providers/FrontendServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Agenciafmd\Frontend\Providers;
 
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class FrontendServiceProvider extends ServiceProvider
@@ -9,6 +10,8 @@ class FrontendServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->providers();
+
+        $this->loadViteMacros();
 
         $this->publish();
     }
@@ -32,5 +35,12 @@ class FrontendServiceProvider extends ServiceProvider
     private function loadConfigs(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../../config/frontend.php', 'frontend');
+    }
+
+    private function loadViteMacros(): void
+    {
+        Vite::macro('image', fn (string $asset) => $this->asset("resources/images/{$asset}"));
+        Vite::macro('font', fn (string $asset) => $this->asset("resources/fonts/{$asset}"));
+        Vite::macro('sprite', fn (string $asset) => $this->asset('spritemap.svg') . "#sprite-{$asset}");
     }
 }
