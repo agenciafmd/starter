ceba16067cbeb161f67a7d99491f3bb4ac84d93c
Irineu Junior <irineu@fmd.ag>
2025-01-25 17:47:25 -0300

    feat(livewiretable) remove a inclusão automatica do css e js

--- config/livewire-tables.php
+++ config/livewire-tables.php
@@ -18,12 +18,12 @@ return [
     /**
      * Enable or Disable automatic injection of core assets
      */
-    'inject_core_assets_enabled' => true,
+    'inject_core_assets_enabled' => false,
 
     /**
      * Enable or Disable automatic injection of third-party assets
      */
-    'inject_third_party_assets_enabled' => true,
+    'inject_third_party_assets_enabled' => false,
 
     /**
      * Enable Blade Directives (Not required if automatically injecting or using bundler approaches)
