63786b79b95287ba0af27a00fc2a515163fb854f
edmosouza <xedmox@gmail.com>
2025-07-15 18:30:05 -0300

    feat(frontend): adiciona novos ícones e melhorias de estilo
    Adiciona ícones de zoom e play para os componentes de imagem e vídeo.
    Melhora a responsividade e a aparência dos elementos de interface.

--- packages/agenciafmd/frontend/resources/views/components/glightbox/image.blade.php
+++ packages/agenciafmd/frontend/resources/views/components/glightbox/image.blade.php
@@ -11,7 +11,11 @@
                   data-description="{{ $description }}"
                   title="{{ $title ? $title : $description }}"
                   aria-label="Link: {{ $title ? $title : $description }}"
-                  {{ $attributes->merge(['class' => 'position-relative text-decoration-none glightbox']) }}>
+                  {{ $attributes->merge(['class' => 'position-relative text-decoration-none glightbox text-primary text-secondary-hover']) }}>
   {{ $slot }}
+
+  <div class="m-0hq position-absolute top-0 end-0 ic-glightbox-zoom">
+    <x-frontend-icon name="ic-ui-expand" class="" />
+  </div>
 </x-frontend::link>
 
