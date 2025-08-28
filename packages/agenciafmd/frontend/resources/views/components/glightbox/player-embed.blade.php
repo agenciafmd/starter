8dd7f1c0f5d1abd1a17b32af70631e5d99f1a064
GustavoCPS22 <gustavocpsalles22@gmail.com>
2025-08-28 14:30:06 -0300

    fix(glightbox videos): Ajusta estrutura da classe de ic-play para prevenir problemas no firefox e safari, e para melhorar legibilidade

--- packages/agenciafmd/frontend/resources/views/components/glightbox/player-embed.blade.php
+++ packages/agenciafmd/frontend/resources/views/components/glightbox/player-embed.blade.php
@@ -39,10 +39,10 @@
                     data-plyr-provider="{{ $provider }}"
                     data-plyr-embed-id="{{ $link }}"
                     id="player-{{ $id }}"
-                    class="stretched-link {{ $iconClass }} js-player-embed-link ic-play"
+                    class="stretched-link {{ $iconClass }} js-player-embed-link ic-play-container"
                     aria-label="Clique para assistir o vídeo"
                     title="Ver vídeo">
-    <x-frontend-icon class="vstack align-items-center justify-content-center rounded-circle icon position-absolute top-50 start-50 translate-middle z-1"
+    <x-frontend-icon class="vstack align-items-center justify-content-center rounded-circle ic-play position-absolute top-50 start-50 translate-middle z-1"
                      name="ic-ui-play"/>
   </x-frontend::link>
 
